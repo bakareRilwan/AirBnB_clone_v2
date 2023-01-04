7bf81c916f5dc9b4c68e34a86216388222d76afc
bakareRilwan <baccrie88@gmail.com>
2023-01-04 05:46:29 -0800

    Task 3 corr

--- user.py
+++ user.py
@@ -16,7 +16,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, line):
         """Quit comand to exit the program"""
-        print()
+        print('')
         return True
 
     def emptyline(self):
